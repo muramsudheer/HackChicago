cc932bfd7f68e213d46bc21ffbaf31e1a597b5a7
muramsudheer <mohammed.maazin@gmail.com>
2018-07-21 14:38:37 -0500

    Created Project

---
+++ FoodShelter/FoodShelter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodShelter
+//
+//  Created by Mohamed Maazin Sudheer on 7/21/18.
+//  Copyright © 2018 Mohamed Sudheer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
